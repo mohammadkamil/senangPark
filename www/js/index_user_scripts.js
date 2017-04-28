dc3bc163ba2d3d75b730c38a49b67d1ee1597e4a
mohammadkamil <mohammadkamil1991@gmail.com>
2017-04-28 18:11:00 +0800

    weweq

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,32 @@
+/*jshint browser:true */
+/*global $ */
+(function () {
+    "use strict";
+    /*
+      hook up event handlers 
+    */
+    function register_event_handlers() {
+
+        /* button  #send */
+        $(document).on("click", "#send", function (evt) {
+            bootbox.alert("DSADSA");
+            /* your code goes here */
+            var notel= $("#notel").val();
+            console.log(notel);
+            var messageInfo = {
+                phoneNumber: notel,
+                textMessage: "This is a test message"
+            };
+
+            sms.sendMessage(messageInfo, function (message) {
+                console.log("success: " + message);
+            }, function (error) {
+                console.log("code: " + error.code + ", message: " + error.message);
+            });
+            
+            return false;
+        });
+
+    }
+    document.addEventListener("app.Ready", register_event_handlers, false);
+})();
